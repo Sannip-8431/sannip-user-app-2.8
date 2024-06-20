896b091ca49a05ea5031cfa31492de12537ce8f1
Adarsh_k <akadrolli@skycliffit.com>
2024-06-20 13:23:53 +0530

    color changes in the light and dark theme

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:sannip/util/app_constants.dart';
 
-ThemeData dark({Color color = const Color(0xFF54b46b)}) => ThemeData(
+// ThemeData dark({Color color = const Color(0xFF54b46b)}) => ThemeData(
+ThemeData dark({Color color = const Color(0xFFCA3746)}) => ThemeData(
       fontFamily: AppConstants.fontFamily,
       primaryColor: color,
-      secondaryHeaderColor: const Color(0xFF009f67),
+      secondaryHeaderColor: const Color(0xFFCC4553),
       disabledColor: const Color(0xffa2a7ad),
       brightness: Brightness.dark,
       hintColor: const Color(0xFFbebebe),
--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:sannip/util/app_constants.dart';
 
-ThemeData light({Color color = const Color(0xFF039D55)}) => ThemeData(
+// ThemeData light({Color color = const Color(0xFF039D55)}) => ThemeData(
+ThemeData light({Color color = const Color(0xFFaa2533)}) => ThemeData(
       fontFamily: AppConstants.fontFamily,
       primaryColor: color,
-      secondaryHeaderColor: const Color(0xFF1ED7AA),
+      secondaryHeaderColor: const Color(0xFFCA3746),
       disabledColor: const Color(0xFFBABFC4),
       brightness: Brightness.light,
       hintColor: const Color(0xFF9F9F9F),
