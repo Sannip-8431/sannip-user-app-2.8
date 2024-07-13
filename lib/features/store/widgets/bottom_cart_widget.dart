a86d4367f7aca6d38490706077d5d3e7625bc804
Adarsh_k <akadrolli@skycliffit.com>
2024-07-13 17:11:31 +0530

    changes in bottom cart widget

--- lib/features/store/widgets/bottom_cart_widget.dart
+++ lib/features/store/widgets/bottom_cart_widget.dart
@@ -13,55 +13,59 @@ class BottomCartWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<CartController>(builder: (cartController) {
-      return Container(
-        height: GetPlatform.isIOS ? 100 : 70,
-        width: Get.width,
-        padding: const EdgeInsets.symmetric(
-          horizontal: Dimensions
-              .paddingSizeExtraLarge, /* vertical: Dimensions.PADDING_SIZE_SMALL*/
-        ),
-        decoration: BoxDecoration(
-          color: Theme.of(context).cardColor,
-          boxShadow: [
-            BoxShadow(
-                color: const Color(0xFF2A2A2A).withOpacity(0.1),
-                blurRadius: 10,
-                offset: const Offset(0, -5))
-          ],
-        ),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('${'item'.tr}: ${cartController.cartList.length}',
-                    style: robotoMedium.copyWith(
-                        fontSize: Dimensions.fontSizeDefault)),
-                const SizedBox(height: Dimensions.paddingSizeExtraSmall),
-                Row(children: [
-                  Text(
-                    '${'total'.tr}: ',
-                    style: robotoMedium.copyWith(
-                        fontSize: Dimensions.fontSizeLarge,
-                        color: Theme.of(context).primaryColor),
-                  ),
-                  Text(
-                    PriceConverter.convertPrice(
-                        cartController.calculationCart()),
-                    style: robotoMedium.copyWith(
-                        fontSize: Dimensions.fontSizeLarge,
-                        color: Theme.of(context).primaryColor),
-                    textDirection: TextDirection.ltr,
-                  ),
+      return InkWell(
+        onTap: () => Get.toNamed(RouteHelper.getCartRoute()),
+        child: Container(
+          height: GetPlatform.isIOS ? 100 : 70,
+          width: Get.width,
+          padding: const EdgeInsets.symmetric(
+            horizontal: Dimensions
+                .paddingSizeExtraLarge, /* vertical: Dimensions.PADDING_SIZE_SMALL*/
+          ),
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            // boxShadow: [
+            //   BoxShadow(
+            //       color: const Color(0xFF2A2A2A).withOpacity(0.1),
+            //       blurRadius: 10,
+            //       offset: const Offset(0, -5))
+            // ],
+          ),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('${cartController.cartList.length} ${'items'.tr}',
+                      style: robotoMedium.copyWith(
+                          color: Colors.white,
+                          fontSize: Dimensions.fontSizeSmall)),
+                  const SizedBox(height: Dimensions.paddingSizeExtraSmall),
+                  Row(children: [
+                    Text(
+                      PriceConverter.convertPrice(
+                          cartController.calculationCart()),
+                      style: robotoMedium.copyWith(
+                          fontSize: Dimensions.fontSizeLarge,
+                          color: Colors.white),
+                      textDirection: TextDirection.ltr,
+                    ),
+                    Text(
+                      ' ${'total'.tr}',
+                      style: robotoMedium.copyWith(
+                          fontSize: Dimensions.fontSizeSmall,
+                          color: Colors.white),
+                    ),
+                  ]),
                 ]),
-              ]),
-          CustomButton(
-              buttonText: 'view_cart'.tr,
-              width: 130,
-              height: 45,
-              onPressed: () => Get.toNamed(RouteHelper.getCartRoute()))
-        ]),
+            CustomButton(
+                buttonText: 'view_cart'.tr,
+                width: 130,
+                height: 45,
+                onPressed: () => Get.toNamed(RouteHelper.getCartRoute()))
+          ]),
+        ),
       );
     });
   }
