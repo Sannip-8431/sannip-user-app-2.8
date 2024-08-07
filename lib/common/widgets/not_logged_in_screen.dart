da4aaa5642ba5a29d922847e78981d231fbaf17b
Adarsh_k <akadrolli@skycliffit.com>
2024-08-07 21:18:45 +0530

    changes in not logged in screen

--- lib/common/widgets/not_logged_in_screen.dart
+++ lib/common/widgets/not_logged_in_screen.dart
@@ -1,7 +1,9 @@
+import 'package:lottie/lottie.dart';
 import 'package:sannip/features/auth/screens/sign_in_screen.dart';
 import 'package:sannip/features/order/controllers/order_controller.dart';
 import 'package:sannip/helper/responsive_helper.dart';
 import 'package:sannip/helper/route_helper.dart';
+import 'package:sannip/util/dimensions.dart';
 import 'package:sannip/util/images.dart';
 import 'package:sannip/util/styles.dart';
 import 'package:sannip/common/widgets/custom_button.dart';
@@ -15,57 +17,69 @@ class NotLoggedInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: FooterView(
-        child: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  Images.guest,
-                  width: MediaQuery.of(context).size.height * 0.25,
-                  height: MediaQuery.of(context).size.height * 0.25,
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-                Text(
-                  'you_are_not_logged_in'.tr,
-                  style: robotoBold.copyWith(
-                      fontSize: MediaQuery.of(context).size.height * 0.023),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-                Text(
-                  'please_login_to_continue'.tr,
-                  style: robotoRegular.copyWith(
-                      fontSize: MediaQuery.of(context).size.height * 0.0175,
-                      color: Theme.of(context).disabledColor),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height * 0.04),
-                SizedBox(
-                  width: 200,
-                  child: CustomButton(
-                      buttonText: 'login'.tr,
-                      height: 40,
-                      onPressed: () async {
-                        if (!ResponsiveHelper.isDesktop(context)) {
-                          await Get.toNamed(
-                              RouteHelper.getSignInRoute(Get.currentRoute));
-                        } else {
-                          Get.dialog(const SignInScreen(
-                                  exitFromApp: true, backFromThis: true))
-                              .then((value) => callBack(true));
-                        }
-                        if (Get.find<OrderController>().showBottomSheet) {
-                          Get.find<OrderController>().showRunningOrders();
-                        }
-                        callBack(true);
-                      }),
-                ),
-              ]),
+    return Column(
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            child: FooterView(
+              child: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Lottie.asset(
+                        Images.notLoggedIn,
+                        width: MediaQuery.of(context).size.height * 0.35,
+                        height: MediaQuery.of(context).size.height * 0.35,
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.01),
+                      Text(
+                        'oops_you_have_not_signed_in'.tr,
+                        style: robotoBold.copyWith(
+                            fontSize:
+                                MediaQuery.of(context).size.height * 0.023),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.01),
+                      Text(
+                        'kindly_sign_in'.tr,
+                        style: robotoRegular.copyWith(
+                            fontSize:
+                                MediaQuery.of(context).size.height * 0.0175,
+                            color: Theme.of(context).disabledColor),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.04),
+                    ]),
+              ),
+            ),
+          ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: Dimensions.paddingSizeLarge),
+          child: CustomButton(
+              width: MediaQuery.of(context).size.width * 0.7,
+              radius: 100,
+              buttonText: 'sign_in'.tr,
+              onPressed: () async {
+                if (!ResponsiveHelper.isDesktop(context)) {
+                  await Get.toNamed(
+                      RouteHelper.getSignInRoute(Get.currentRoute));
+                } else {
+                  Get.dialog(const SignInScreen(
+                          exitFromApp: true, backFromThis: true))
+                      .then((value) => callBack(true));
+                }
+                if (Get.find<OrderController>().showBottomSheet) {
+                  Get.find<OrderController>().showRunningOrders();
+                }
+                callBack(true);
+              }),
+        ),
+      ],
     );
   }
 }
