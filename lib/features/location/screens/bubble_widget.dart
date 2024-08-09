b4b260a959623a414e4878db13bb5a05cc6431cf
shriranga sb <sbagalkoti@skycliffit.com>
2024-08-09 11:37:47 +0530

    map bubble widget

---
+++ lib/features/location/screens/bubble_widget.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:sannip/util/dimensions.dart';
+import 'package:sannip/util/styles.dart';
+
+class MapLocationBubbleWidget extends StatelessWidget {
+  const MapLocationBubbleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: SpeechBubbleClipper(),
+      child: Container(
+        color: Colors.black,
+        padding: const EdgeInsets.only(
+            bottom: Dimensions.paddingSizeDefault,
+            left: Dimensions.paddingSizeSmall,
+            top: Dimensions.paddingSizeSmall / 2,
+            right: Dimensions.paddingSizeSmall),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "Order will be delivered here",
+              style: robotoMedium.copyWith(
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              "Place the pin accurately on the map",
+              style: robotoRegular.copyWith(
+                color: Colors.white70,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SpeechBubbleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    const double radius = 8.0;
+    const double pointerWidth = 20.0;
+    const double pointerHeight = 15.0;
+
+    Path path = Path()
+      ..moveTo(radius, 0)
+      ..lineTo(size.width - radius, 0)
+      ..arcToPoint(
+        Offset(size.width, radius),
+        radius: const Radius.circular(radius),
+      )
+      ..lineTo(size.width, size.height - radius - pointerHeight)
+      ..arcToPoint(
+        Offset(size.width - radius, size.height - pointerHeight),
+        radius: const Radius.circular(radius),
+      )
+      ..lineTo(size.width / 2 + pointerWidth / 2, size.height - pointerHeight)
+      ..lineTo(size.width / 2, size.height)
+      ..lineTo(size.width / 2 - pointerWidth / 2, size.height - pointerHeight)
+      ..lineTo(radius, size.height - pointerHeight)
+      ..arcToPoint(
+        Offset(0, size.height - radius - pointerHeight),
+        radius: const Radius.circular(radius),
+      )
+      ..lineTo(0, radius)
+      ..arcToPoint(
+        const Offset(radius, 0),
+        radius: const Radius.circular(radius),
+      )
+      ..close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
--- lib/features/location/screens/pick_map_screen.dart
+++ lib/features/location/screens/pick_map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:sannip/common/widgets/custom_app_bar.dart';
 import 'package:sannip/features/location/controllers/location_controller.dart';
+import 'package:sannip/features/location/screens/bubble_widget.dart';
 import 'package:sannip/features/splash/controllers/splash_controller.dart';
 import 'package:sannip/features/profile/controllers/profile_controller.dart';
 import 'package:sannip/features/address/domain/models/address_model.dart';
@@ -8,7 +9,6 @@ import 'package:sannip/helper/address_helper.dart';
 import 'package:sannip/helper/auth_helper.dart';
 import 'package:sannip/helper/responsive_helper.dart';
 import 'package:sannip/util/dimensions.dart';
-import 'package:sannip/util/images.dart';
 import 'package:sannip/util/styles.dart';
 import 'package:sannip/common/widgets/custom_button.dart';
 import 'package:sannip/common/widgets/custom_snackbar.dart';
@@ -121,6 +121,7 @@ class _PickMapScreenState extends State<PickMapScreen> {
                             borderRadius:
                                 BorderRadius.circular(Dimensions.radiusDefault),
                             child: GoogleMap(
+                              cloudMapId: "2daff71eef425386",
                               initialCameraPosition: CameraPosition(
                                 target: widget.fromAddAddress
                                     ? LatLng(
@@ -157,8 +158,9 @@ class _PickMapScreenState extends State<PickMapScreen> {
                           ),
                           Center(
                               child: !locationController.loading
-                                  ? Image.asset(Images.pickMarker,
-                                      height: 50, width: 50)
+                                  ? /* Image.asset(Images.pickMarker,
+                                      height: 50, width: 50) */
+                                  const MapLocationBubbleWidget()
                                   : const CircularProgressIndicator()),
                           Positioned(
                             bottom: 30,
@@ -308,41 +310,46 @@ class _PickMapScreenState extends State<PickMapScreen> {
                   ),
                 )
               : Stack(children: [
-                  GoogleMap(
-                    cloudMapId: "2daff71eef425386",
-                    initialCameraPosition: CameraPosition(
-                      target: widget.fromAddAddress
-                          ? LatLng(locationController.position.latitude,
-                              locationController.position.longitude)
-                          : _initialPosition,
-                      zoom: 16,
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 150),
+                    child: GoogleMap(
+                      cloudMapId: "2daff71eef425386",
+                      initialCameraPosition: CameraPosition(
+                        target: widget.fromAddAddress
+                            ? LatLng(locationController.position.latitude,
+                                locationController.position.longitude)
+                            : _initialPosition,
+                        zoom: 16,
+                      ),
+                      minMaxZoomPreference: const MinMaxZoomPreference(0, 30),
+                      myLocationButtonEnabled: false,
+                      onMapCreated: (GoogleMapController mapController) {
+                        _mapController = mapController;
+                        if (!widget.fromAddAddress) {
+                          Get.find<LocationController>().getCurrentLocation(
+                              false,
+                              mapController: mapController);
+                        }
+                      },
+                      scrollGesturesEnabled: /* !Get.isDialogOpen! */ true,
+                      zoomControlsEnabled: false,
+                      onCameraMove: (CameraPosition cameraPosition) {
+                        _cameraPosition = cameraPosition;
+                      },
+                      onCameraMoveStarted: () {
+                        locationController.disableButton();
+                      },
+                      onCameraIdle: () {
+                        Get.find<LocationController>()
+                            .updatePosition(_cameraPosition, false);
+                      },
                     ),
-                    minMaxZoomPreference: const MinMaxZoomPreference(0, 30),
-                    myLocationButtonEnabled: false,
-                    onMapCreated: (GoogleMapController mapController) {
-                      _mapController = mapController;
-                      if (!widget.fromAddAddress) {
-                        Get.find<LocationController>().getCurrentLocation(false,
-                            mapController: mapController);
-                      }
-                    },
-                    scrollGesturesEnabled: /* !Get.isDialogOpen! */ true,
-                    zoomControlsEnabled: false,
-                    onCameraMove: (CameraPosition cameraPosition) {
-                      _cameraPosition = cameraPosition;
-                    },
-                    onCameraMoveStarted: () {
-                      locationController.disableButton();
-                    },
-                    onCameraIdle: () {
-                      Get.find<LocationController>()
-                          .updatePosition(_cameraPosition, false);
-                    },
                   ),
                   Center(
                       child: !locationController.loading
-                          ? Image.asset(Images.pickMarker,
-                              height: 50, width: 50)
+                          ? /*  Image.asset(Images.pickMarker,
+                              height: 50, width: 50) */
+                          const MapLocationBubbleWidget()
                           : const CircularProgressIndicator()),
                   Positioned(
                     top: Dimensions.paddingSizeLarge,
@@ -350,11 +357,11 @@ class _PickMapScreenState extends State<PickMapScreen> {
                     right: Dimensions.paddingSizeSmall,
                     child: SearchLocationWidget(
                         mapController: _mapController,
-                        pickedAddress: locationController.pickAddress,
+                        pickedAddress: '',
                         isEnabled: null),
                   ),
                   Positioned(
-                    bottom: 100,
+                    bottom: 160,
                     left: Get.width / 3,
                     right: Get.width / 3,
                     child: FloatingActionButton.extended(
@@ -374,86 +381,129 @@ class _PickMapScreenState extends State<PickMapScreen> {
                     ),
                   ),
                   Positioned(
-                    bottom: Dimensions.paddingSizeLarge,
-                    left: Dimensions.paddingSizeLarge,
-                    right: Dimensions.paddingSizeLarge,
-                    child: CustomButton(
-                      buttonText: locationController.inZone
-                          ? widget.fromAddAddress
-                              ? 'pick_address'.tr
-                              : 'pick_location'.tr
-                          : 'service_not_available_in_this_area'.tr,
-                      isLoading: locationController.isLoading,
-                      onPressed: locationController.isLoading
-                          ? () {}
-                          : (locationController.buttonDisabled ||
-                                  locationController.loading)
-                              ? null
-                              : () {
-                                  if (locationController
-                                              .pickPosition.latitude !=
-                                          0 &&
-                                      locationController
-                                          .pickAddress!.isNotEmpty) {
-                                    if (widget.onPicked != null) {
-                                      AddressModel address = AddressModel(
-                                        latitude: locationController
-                                            .pickPosition.latitude
-                                            .toString(),
-                                        longitude: locationController
-                                            .pickPosition.longitude
-                                            .toString(),
-                                        addressType: 'others',
-                                        address: locationController.pickAddress,
-                                        contactPersonName: AddressHelper
-                                                .getUserAddressFromSharedPref()!
-                                            .contactPersonName,
-                                        contactPersonNumber: AddressHelper
-                                                .getUserAddressFromSharedPref()!
-                                            .contactPersonNumber,
-                                      );
-                                      widget.onPicked!(address);
-                                      Get.back();
-                                    } else if (widget.fromAddAddress) {
-                                      if (widget.googleMapController != null) {
-                                        widget.googleMapController!.moveCamera(
-                                            CameraUpdate.newCameraPosition(
-                                                CameraPosition(
-                                                    target: LatLng(
-                                                      locationController
-                                                          .pickPosition
-                                                          .latitude,
-                                                      locationController
-                                                          .pickPosition
-                                                          .longitude,
-                                                    ),
-                                                    zoom: 16)));
-                                        locationController.setAddAddressData();
-                                      }
-                                      Get.back();
-                                    } else {
-                                      AddressModel address = AddressModel(
-                                        latitude: locationController
-                                            .pickPosition.latitude
-                                            .toString(),
-                                        longitude: locationController
-                                            .pickPosition.longitude
-                                            .toString(),
-                                        addressType: 'others',
-                                        address: locationController.pickAddress,
-                                      );
-                                      locationController.saveAddressAndNavigate(
-                                        address,
-                                        widget.fromSignUp,
-                                        widget.route,
-                                        widget.canRoute,
-                                        ResponsiveHelper.isDesktop(context),
-                                      );
-                                    }
-                                  } else {
-                                    showCustomSnackBar('pick_an_address'.tr);
-                                  }
-                                },
+                    bottom: 0,
+                    child: Container(
+                      height: 150,
+                      width: Get.width,
+                      color: Theme.of(context).cardColor,
+                      padding:
+                          const EdgeInsets.all(Dimensions.paddingSizeSmall),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  const Icon(Icons.location_pin),
+                                  Flexible(
+                                    child: Text(
+                                      extractLocation(
+                                          locationController.pickAddress ?? ''),
+                                      style: robotoBold.copyWith(
+                                          fontSize:
+                                              Dimensions.fontSizeExtraLarge),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                locationController.pickAddress ?? '',
+                                maxLines: 2,
+                                style: robotoRegular.copyWith(
+                                    fontSize: Dimensions.fontSizeDefault),
+                              ),
+                            ],
+                          ),
+                          CustomButton(
+                            buttonText: locationController.inZone
+                                ? widget.fromAddAddress
+                                    ? 'pick_address'.tr
+                                    : 'pick_location'.tr
+                                : 'service_not_available_in_this_area'.tr,
+                            isLoading: locationController.isLoading,
+                            onPressed: locationController.isLoading
+                                ? () {}
+                                : (locationController.buttonDisabled ||
+                                        locationController.loading)
+                                    ? null
+                                    : () {
+                                        if (locationController
+                                                    .pickPosition.latitude !=
+                                                0 &&
+                                            locationController
+                                                .pickAddress!.isNotEmpty) {
+                                          if (widget.onPicked != null) {
+                                            AddressModel address = AddressModel(
+                                              latitude: locationController
+                                                  .pickPosition.latitude
+                                                  .toString(),
+                                              longitude: locationController
+                                                  .pickPosition.longitude
+                                                  .toString(),
+                                              addressType: 'others',
+                                              address: locationController
+                                                  .pickAddress,
+                                              contactPersonName: AddressHelper
+                                                      .getUserAddressFromSharedPref()!
+                                                  .contactPersonName,
+                                              contactPersonNumber: AddressHelper
+                                                      .getUserAddressFromSharedPref()!
+                                                  .contactPersonNumber,
+                                            );
+                                            widget.onPicked!(address);
+                                            Get.back();
+                                          } else if (widget.fromAddAddress) {
+                                            if (widget.googleMapController !=
+                                                null) {
+                                              widget.googleMapController!
+                                                  .moveCamera(CameraUpdate
+                                                      .newCameraPosition(
+                                                          CameraPosition(
+                                                              target: LatLng(
+                                                                locationController
+                                                                    .pickPosition
+                                                                    .latitude,
+                                                                locationController
+                                                                    .pickPosition
+                                                                    .longitude,
+                                                              ),
+                                                              zoom: 16)));
+                                              locationController
+                                                  .setAddAddressData();
+                                            }
+                                            Get.back();
+                                          } else {
+                                            AddressModel address = AddressModel(
+                                              latitude: locationController
+                                                  .pickPosition.latitude
+                                                  .toString(),
+                                              longitude: locationController
+                                                  .pickPosition.longitude
+                                                  .toString(),
+                                              addressType: 'others',
+                                              address: locationController
+                                                  .pickAddress,
+                                            );
+                                            locationController
+                                                .saveAddressAndNavigate(
+                                              address,
+                                              widget.fromSignUp,
+                                              widget.route,
+                                              widget.canRoute,
+                                              ResponsiveHelper.isDesktop(
+                                                  context),
+                                            );
+                                          }
+                                        } else {
+                                          showCustomSnackBar(
+                                              'pick_an_address'.tr);
+                                        }
+                                      },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ]);
@@ -461,4 +511,11 @@ class _PickMapScreenState extends State<PickMapScreen> {
       ))),
     );
   }
+
+  String extractLocation(String address) {
+    // Split the string by commas and return the first part
+    return address.split(',')[0].trim();
+  }
 }
+
+class ISpeechBubbleWidget {}
